8144d33e338797c4ac76c704f07321d041062a8a
wldnjd2 <jeewon3665@naver.com>
2021-12-09 23:54:18 +0900

    python

--- python_basic/step05_8_basic.py
+++
@@ -1,6 +0,0 @@
-# /c/ProgramData/Anaconda3/python
-# -*- coding: utf-8 -*-
-
-# context manager 패턴
-# open / close, Lock / Release, Change / Reset, Enter / Exit, Start / Stop, Setup / Teardown, Connect / Disconnect
-
